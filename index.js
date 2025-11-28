9dca36c9a27708ba7f052ce5273f5c7f434ec68a
Haiqal <haiqalaly@gmail.com>
2025-11-28 08:46:21 +0700

    feat: implement product update endpoint

--- index.js
+++ index.js
@@ -34,12 +34,27 @@ app.get('/api/products/:id', async (req, res) => {
   }
 })
 
-
-
 app.post('/api/products', async (req, res) => {
   try {
     const product = await Product.create(req.body)
     res.status(201).json(product)
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
+})
+
+// Update a product
+app.put('/api/products/:id', async (req, res) => {
+  try {
+    const {id} = req.params
+    const product =await Product.findByIdAndUpdate(id, req.body)
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product Not Found' })
+    }
+
+    const updatedProduct = await Product.findById(id)
+    res.status(200).json(updatedProduct)
   } catch (error) {
     res.status(500).json({ message: 'Internal Server Error' })
   }
